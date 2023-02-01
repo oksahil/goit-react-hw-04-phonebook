1fe30ad146a1eaddab0741cb19a8950597d7f3e9
oksahil <oksana.khylyk@gmail.com>
2023-02-01 22:27:51 +0100

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './index.css';
 
+import MainMenu from 'modules/MainMenu/MainMenu';
+import mainMenu from "./data/mainMenu.json";
 import MyPhone from 'modules/MyPhone/MyPhone';
 
 function App() {
   return (
     <div className="App">
+      <MainMenu items={mainMenu} />
       <MyPhone />
     </div>
   );
